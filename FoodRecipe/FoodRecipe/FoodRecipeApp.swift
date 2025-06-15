deb43c9efcab649b8936dafef89d993bef80c103
ABDULLAH HAFIZ <ABDULLAH_MH30@OUTLOOK.COM>
2025-06-15 17:13:34 +0300

    created some files

---
+++ FoodRecipe/FoodRecipe/FoodRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecipeApp.swift
+//  FoodRecipe
+//
+//  Created by Abdullah Hafiz on 15/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodRecipe/FoodRecipe/Recipe.swift
@@ -0,0 +1,12 @@
+//
+//  Recipe.swift
+//  FoodRecipe
+//
+//  Created by Abdullah Hafiz on 15/06/2025.
+//
+
+import SwiftUI
+
+struct recipe: Codable {
+    
+}
